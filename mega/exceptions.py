bc2229d6e5bb9d7a3258ca6448c06f8f9d8a218c
kaj117 <70688258+kaj117@users.noreply.github.com>
2021-08-01 18:26:28 +0530

    Create exceptions.py

---
+++ mega/exceptions.py
@@ -0,0 +1,13 @@
+class MegaException(Exception):
+    pass
+    
+class MegaIncorrectPasswordExcetion(MegaException):
+    """
+    A incorrect password or email was given.
+    """
+
+class MegaRequestException(MegaException):
+    """
+    There was an error in the request.
+    """
+    pass
